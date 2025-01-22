44d3c3a29fa20102b8989c274bd1c992d2170af0
Lê Sỹ Tuyền <130513663+Hatohui@users.noreply.github.com>
2025-01-22 20:51:44 +0700

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -73,9 +73,8 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command: 'start-prod',
+    command: 'npm run start',
     url: 'http://127.0.0.1:3000',
-    timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
   },
 })
