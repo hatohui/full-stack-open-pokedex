60f2d7457eaf4121b3a152e020fe04972785351d
Lê Sỹ Tuyền <130513663+Hatohui@users.noreply.github.com>
2025-01-22 21:17:00 +0700

    Update frontpage.spec.js

--- tests/frontpage.spec.js
+++ tests/frontpage.spec.js
@@ -10,4 +10,13 @@ describe('Pokedex', () => {
       )
     ).toBeVisible()
   })
+
+  test('you can navigate to specific pokemons', async ({ page }) => {
+    await page.goto('/')
+    const locator = await page.locator('a').locator('text=Bulbasaur')
+    await page.click(locator)
+    await page.waitForNavigation()
+    await expect(page).toHaveURL('/pokemon/bulbasaur')
+    await expect(page.getByText('chlorophyll')).toBeVisible()
+  })
 })
